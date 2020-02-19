fbdba15f7ee4332c34c6cdff7334bf4336cb1776
jay-izzal <60599395+jay-izzal@users.noreply.github.com>
2020-02-19 20:45:35 +1300

    Update welcome_message.php

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -71,15 +71,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<h1>Congrats! You just created your first PHP web app in Azure App Service (Powered by CodeIgniter)!</h1>
 
 	<div id="body">
-		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
+		<p>This page could be the artsonline portion of ikt.</p>
 
-		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
+		<p>See how easy it is to deploy a php app? We could even look at PHP5 to PHP7 conversion</p>
+		
+		<p>Comes with MySQL database as requested</p>
 
 		<p>The corresponding controller for this page is found at:</p>
 		<code>application/controllers/Welcome.php</code>
 
-		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
+		<p>BTW, this stuff is really fun to play with.  I'm enjoying the self development.</p>
 	</div>
 
 	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
